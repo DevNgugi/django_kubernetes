319fce4177fafeddf46baafd0420c73327dbf3a7
devngugi <philemonngugi18@gmail.com>
2023-10-28 13:33:26 +0300

    docker compose optimization

--- migrate.sh
+++ migrate.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-'admin@app.com'}
+USERNAME=${DJANGO_SUPERUSER_NAME:-'admin'}
 
 cd /app/
 
 
 /opt/venv/bin/python manage.py migrate --noinput
 # if superuser exists, the || makes sure it will not return errors
-/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
+/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --username $USERNAME --noinput || true
 
 
  
